9cbf68bf129f896d7e7a216a346eb0558b7ec72d
jackstolt <jackstolt@gmail.com>
2022-04-28 20:28:35 -0600

    recurrent, adding models, bettering everything because I am the goat

--- NEAT/NEAT_racecar.py
+++ NEAT/NEAT_racecar.py
@@ -1,5 +1,5 @@
+from processing import rgb2gray, collision, lidar
 import itertools as it
-from tabnanny import verbose
 import numpy as np
 import pickle
 import neat
@@ -13,145 +13,6 @@ VERBOSE = False
 TEST = False
 
 
-def rgb2gray(rgb):
-    '''
-    Process image from color to grayscale
-    -rgb -> color observation to be converted
-    '''
-    gray = np.dot(rgb[..., :], [0.299, 0.587, 0.114])
-    gray = gray / 255
-    return gray.round().astype(int)
-
-
-def collision(obs, frame):
-    '''
-    Check to see if car has gone off the track
-    -obs -> grayscale observation of shape (96,84) 
-    -frame -> current frame being checked
-    '''
-    if frame < 20: 
-        return False
-    
-    car = 0
-    for y in range(66, 77): 
-        for x in range(45,51):
-            car += obs[y][x]
-
-    return False if car <= 1 else True
-
-
-def lidar(frame, env):
-    '''
-    Get distances from car to track (left, right, and front)
-    '''
-    frame = frame[:84]
-    left, right, front = 0, 0, 0
-    for x in range(44,0,-1):
-        if not frame[66][x]:
-            left += 1
-        else: break
-    for x in range(51,96):
-        if not frame[66][x]:
-            right += 1
-        else: break
-    for y in range(67,-1,-1):
-        if not frame[y][47]:
-            front += 1
-        else: break
-
-    f_left, f_right = 0, 0
-    for y in range(45,-1,-1):
-        if not frame[y+19][y]:
-            f_left += 1
-        else: break
-
-    for y, y2 in enumerate(range(65,96-66,-1)):
-        if not frame[y2][y+50]:
-            f_right += 1
-        else: break
-    
-    l_front, r_front = 0, 0
-    for y in range(66,-1,-2):
-        if not frame[y][y//2+11]:
-            l_front += 1
-        else: break
-    for y, y2 in enumerate(range(66,-1,-2)):
-        if not frame[y2][y+51]:
-            r_front += 1
-        else: break
-    speed = np.sqrt(np.square(env.car.hull.linearVelocity[0]) + np.square(env.car.hull.linearVelocity[1]))
-    return tuple([speed, left, right, front, f_left, f_right, l_front, r_front])
-
-
-def print_obs(obs, x_min=0, x_max=96, y_min=0, y_max=96):
-    '''
-    Print a specific section of an observation
-    '''
-    obs[76][45] = 2
-    obs[66][45] = 2
-    obs[76][50] = 2
-    obs[66][50] = 2
-    for y in range(y_min, y_max):
-        for x in range(x_min, x_max):
-            print('{:.0f} '.format(obs[y][x]), end='')
-        print()
-def test():
-    '''
-    Put in place to test mechanics of the game
-    '''
-    print('Start Game!')
-    env = gym.make('CarRacing-v1', )
-    ep = 0
-    obs = env.reset()
-
-    # action = [1,1,0]
-    actions = [[0,1,0] for i in range(40)]
-    for i in range(100):
-        actions.append([0,0,0])
-    obs = rgb2gray(obs)
-    
-
-    # while not collision(obs, ep):
-    for action in actions:
-        ep += 1
-        obs, r, _, _ = env.step(action)
-        obs = rgb2gray(obs)
-        env.render()
-        # if ep > 30:
-        #     print_obs(obs, 0,96,0,84)
-        #     print(lidar(obs, env))
-        #     return
-
-        print(ep)
-def flip(env):
-    '''
-    Put in place to test mechanics of the game
-    '''
-    actions = []
-    for i in range(250):
-        if i < 20:
-            actions.append([0,1,0])
-        elif i < 40:
-            actions.append([1,1,0])
-        elif i < 70:
-            actions.append([0,0,.2])
-        elif i < 82:
-            actions.append([0,1,0])
-        elif i < 120:
-            actions.append([-1,.9,.1])
-        elif i < 143:
-            actions.append([-1,.5,.1])
-        elif i < 170:
-            actions.append([0,0,.2])
-        elif i < 250:
-            actions.append([0,0,0])
-
-    for action in actions:
-        obs, _, _, _ = env.step(action)
-        obs = rgb2gray(obs)
-        env.render()
-
-
 def eval_genomes(genomes, config):
     '''
     Evaluates the genomes based on the config file settings.
@@ -160,61 +21,60 @@ def eval_genomes(genomes, config):
     '''
 
     print('Evaluating genomes...')
-    env = gym.make('CarRacing-v1', verbose=False)
+    env = gym.make('CarRacing-v1', verbose=VERBOSE)
     
-    scores = []
     # create action space
-    # turn = [-1, -.5, -.1, 0, .1, .5, 1]
-    # gas = [1, .5]
-    # brake = [0, .1, .2]
-    # actions = np.array([action for action in it.product(turn, gas, brake)])
-    actions = [[-1, 0, 0], [-.5, 0, 0], [-.1, 0, 0], [.1, 0, 0], [.5, 0, 0], [1, 0, 0], [0, 1, 0], [0, 0, .2]]
+    actions = [[0, 1, 0], [1, 0, 0], [-1, 0, 0], [0, 0, 0], [0, 0, .2]]
     
     # loop through genomes
     for id, g in genomes:
         score = 0
         g.fitness = 0
-        net = neat.nn.FeedForwardNetwork.create(g, config)
+        net = neat.nn.RecurrentNetwork.create(g, config)
         
         obs = env.reset()
         obs = rgb2gray(obs)
-
+        level = 0
         for step in range(1000):
+
             # determine action based on observation
-            action_idx = np.argmax(net.activate(lidar(obs, env)))
+            inputs = lidar(obs, env)
+            speed = inputs[0]
+            action = actions[np.argmax(net.activate(inputs))]
 
             # make action
-            obs_, reward, _, _ = env.step(actions[action_idx])
+            obs_, reward, _, _ = env.step(action)
             obs = rgb2gray(obs_)
             if RENDER:
                 env.render()
             if VERBOSE:
-                print(actions[action_idx])
-                print('step {}: {:.2f}'.format(step, lidar(obs, env)[0]))
+                print(action)
+                print('step {} -> speed {:.2f}'.format(step, inputs[0]))
 
             # calculate fitness
-            # g.fitness += reward                       # mimic score
+            g.fitness += rewards
+            if score // 50 != level:             # ever increasing checkpoint rewards
+                g.fitness += score // 50 * 25
+                level = score // 50
 
-            # g.fitness += sum(lidar(obs, env)) / 200   # reward keeping distance from sides
-            speed = lidar(obs, env)[0]
-            g.fitness += speed / 100      # reward speed
-            # g.fitness += (actions[action_idx][1] - actions[action_idx][2]) * (score/900)
+            score += reward     # calculate current genomes score individually
             
-            score += reward
-            
-            if step > 50 and speed == 0:
+            if step > 50 and speed < 5:     # terminate if insufficient speed
                 g.fitness -= 500
                 print('Genome -> {}\tFitness -> {:.2f}\tScore -> {:.2f}'.format(id, g.fitness, score))
                 break
-            if collision(obs, step):
+            if collision(obs, step):        # terminate if crashed
                 g.fitness -= 500
-                print('Genome -> {}\tFitness -> {:.2f}\tScore -> {:.2f}'.format(id, g.fitness, score))
+                print('Genome -> {}\tFitness -> {:.2f}\tScore -> {:.2f}   Speed -> {:.2f}'.format(id, g.fitness, score, speed))
                 break
-
+            if step == 999:                 # reward surviving for the duration of the episode
+                g.fitness += 100
+                print('TIMED OUT!\tGenome -> {}\tFitness -> {:.2f}\tScore -> {:.2f}'.format(id, g.fitness, score))
+                
         scores.append(score)
 
 
-def run(config):
+def run(config, check=0):
     '''
     Actually runs a generation and calls the eval_genomes function to evaluate it
     config -> the desired config file
@@ -222,7 +82,7 @@ def run(config):
     print('Generating genetic matter...')
     
     if CHECKPOINT:
-        p = neat.Checkpointer.restore_checkpoint('neat-checkpoint-2')
+        p = neat.Checkpointer.restore_checkpoint('neat-checkpoint-' + check)
     else:
         p = neat.Population(config)
 
@@ -232,43 +92,42 @@ def run(config):
     p.add_reporter(stats)
     p.add_reporter(neat.Checkpointer(1))
 
-    winner = p.run(eval_genomes, 100)
+    best = p.run(eval_genomes, 100)
     with open('best.pickle', 'wb') as f:
-        pickle.dump(winner, f)
+        pickle.dump(best, f)
 
-    print('\nBest genome:\n{!s}'.format(winner))
+    print('\nBest genome:\n{!s}'.format(best))
 
 
-def test_agent(config, file='best.pickle'):
+def test_agent(config, file='recurrent.pickle'):
     with open(file, 'rb') as f:
-        winner = pickle.load(f)
+        best = pickle.load(f)
 
     env = gym.make('CarRacing-v1') 
     step = 0
     score = 0
-    turn = [-1, -.5, -.1, 0, .1, .5, 1]
-    gas = [.5]
-    brake = [0, .1, .2]
-    actions = np.array([action for action in it.product(turn, gas, brake)])
+    actions = [[0, 1, 0], [1, 0, 0], [-1, 0, 0], [0, 0, 0], [0, 0, .2]]
 
-    net = neat.nn.FeedForwardNetwork.create(winner, config)
+    net = neat.nn.RecurrentNetwork.create(best, config)
     obs = env.reset()
     obs = rgb2gray(obs)
 
-    for t in range(1000):
+    for step in range(1000):
         # determine action based on observation
-        action_idx = np.argmax(net.activate(lidar(obs, env)))
+        inputs = lidar(obs, env)
+        speed = inputs[0]
+        action = actions[np.argmax(net.activate(inputs))]
 
         # make action
-        obs_, reward, _, _ = env.step(actions[action_idx])
+        obs_, reward, _, _ = env.step(action)
         obs = rgb2gray(obs_)
         env.render()
         
         score += reward
         if collision(obs, step):
-            print('Score -> {:.2f}'.format(score))
+            print('Speed -> {:.2f}'.format(speed), end=' ')
             break
-        step += 1
+
     print('Score -> {:.2f}'.format(score))
     return score
 
@@ -287,16 +146,18 @@ if __name__ == '__main__':
             RENDER = True
         if(arg == '-c'):
             CHECKPOINT = True
+            idx = sys.argv.index('-c')
+            run(config, check=sys.argv[idx+1])
+            exit(0)
         if(arg == '-v'):
             VERBOSE = True
         if(arg == '-t'):
+            idx = sys.argv.index('-t')
+            file = sys.argv[idx+1]
             scores = []
-            for _ in range(5):
+            for _ in range(10):
                 scores.append(test_agent(config))
             print('Average Score -> {:.2f}'.format(np.mean(scores)))
-            exit(0)
-        if(arg == '-d'):
-            test()
             exit(0)
 
     run(config)
